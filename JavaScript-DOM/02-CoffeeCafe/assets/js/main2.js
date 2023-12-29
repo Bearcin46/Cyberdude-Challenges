ea4b0aae3409d2d574aa0a13fa9968d1af7e53a9
Bearcin46 <imsweety33@gmail.com>
2023-12-29 17:59:25 +0530

    added logo

--- JavaScript-DOM/02-CoffeeCafe/assets/js/main2.js
+++ JavaScript-DOM/02-CoffeeCafe/assets/js/main2.js
@@ -1,8 +1,9 @@
-function hamburger() {
+const hamburgerBtn = document.getElementById("hamburger");
+hamburgerBtn.addEventListener("click", () => {
   let x = document.getElementById("links");
   if (x.classList.contains("hidden")) {
     x.classList.remove("hidden");
   } else {
     x.classList.add("hidden");
   }
-}
+});
