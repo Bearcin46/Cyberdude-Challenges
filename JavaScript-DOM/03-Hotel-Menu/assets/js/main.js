7958d99495e98868682eb38d1a44986b9d79d64a
Bearcin46 <imsweety33@gmail.com>
2024-01-08 14:59:28 +0530

    updated

--- JavaScript-DOM/03-Hotel-Menu/assets/js/main.js
+++ JavaScript-DOM/03-Hotel-Menu/assets/js/main.js
@@ -37,9 +37,58 @@ menulist.forEach((menu) => {
   const price = document.createElement("h3");
   price.setAttribute("class", "font-bold ");
   price.innerText = menu.foodPrice;
-  priceDiv.setAttribute("class", "mt-1 mb-1");
+  priceDiv.setAttribute("class", "mt-1 mb-1 flex gap-2 pr-7");
   priceDiv.appendChild(price);
 
+  //if free delievery feature it
+  if (menu.delievery === "free") {
+    const deliveryDiv = document.createElement("div");
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("width", "26");
+    svg.setAttribute("height", "26");
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("class", "text-[#e01b70ff]");
+
+    // Create the first path element
+    var path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path1.setAttribute("fill", "currentColor");
+    path1.setAttribute(
+      "d",
+      "M19 7c0-1.1-.9-2-2-2h-3v2h3v2.65L13.52 14H10V9H6c-2.21 0-4 1.79-4 4v3h2c0 1.66 1.34 3 3 3s3-1.34 3-3h4.48L19 10.35V7z"
+    );
+
+    // Create the second path element
+    var path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path2.setAttribute("fill", "white");
+    path2.setAttribute(
+      "d",
+      "M4 14v-1c0-1.1.9-2 2-2h2v3H4zm3 3c-.55 0-1-.45-1-1h2c0 .55-.45 1-1 1z"
+    );
+
+    // Create the third path element
+    var path3 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path3.setAttribute("fill", "currentColor");
+    path3.setAttribute(
+      "d",
+      "M5 6h5v2H5zm14 7c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3zm0 4c-.55 0-1-.45-1-1s.45-1 1-1s1 .45 1 1s-.45 1-1 1z"
+    );
+
+    // Append the path elements to the SVG
+    svg.appendChild(path1);
+    svg.appendChild(path2);
+    svg.appendChild(path3);
+    //span tag
+    const spanEl = document.createElement("span");
+    spanEl.setAttribute("class", "text-[#e01b70ff] text-[14px] font-semibold");
+    spanEl.textContent = "Free Delievery";
+
+    //append to div
+    deliveryDiv.append(svg, spanEl);
+    deliveryDiv.setAttribute("class", "flex items-center");
+    //append with price
+    priceDiv.append(deliveryDiv);
+  }
   foodDetails.append(headDiv, priceDiv);
   //svg
   const svgElement = document.createElementNS(
