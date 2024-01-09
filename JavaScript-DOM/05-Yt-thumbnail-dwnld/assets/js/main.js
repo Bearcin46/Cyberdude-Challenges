7084eb163f577437d3dfdf5053dfc97132aff7f3
Bearcin46 <imsweety33@gmail.com>
2024-01-09 11:13:01 +0530

    updated

--- JavaScript-DOM/05-Yt-thumbnail-dwnld/assets/js/main.js
+++ JavaScript-DOM/05-Yt-thumbnail-dwnld/assets/js/main.js
@@ -1,28 +1,39 @@
 const inputUrlEl = document.getElementById("inputUrl");
+const imgEl = document.getElementById("image");
+const linkTag = document.getElementById("linkTag");
+
 inputUrlEl.addEventListener("change", (e) => {
   e.preventDefault();
   const input = e.target.value;
+  const headTag = input.substring(0, 17);
+  const headTag2 = input.substring(0, 32);
 
-  const subStringVal = input.substring(17, 28);
-  const formattedUrl = `https://img.youtube.com/vi/${subStringVal}/sddefault.jpg`;
-  const imgEl = document.getElementById("image");
-  imgEl.src = formattedUrl;
-  const imgDiv = document.getElementById("imgContainer");
-  imgDiv.classList.remove("hidden");
-  const linkTag = document.getElementById("linkTag");
-  linkTag.href = formattedUrl;
+  console.log(headTag2.length);
+  if (headTag === "https://youtu.be/") {
+    const subStringVal = input.substring(17, 28);
+    const formattedUrl = `https://img.youtube.com/vi/${subStringVal}/sddefault.jpg`;
+    imgEl.src = formattedUrl;
+    const imgDiv = document.getElementById("imgContainer");
+    imgDiv.classList.remove("hidden");
+    linkTag.href = formattedUrl;
+  } else if (headTag2 === "https://www.youtube.com/watch?v=") {
+    const live = input.substring(32, 43);
+    const formattedLiveUrl = `https://img.youtube.com/vi/${live}/sddefault.jpg`;
+    imgEl.src = formattedLiveUrl;
+    const imgDiv = document.getElementById("imgContainer");
+    imgDiv.classList.remove("hidden");
+    linkTag.href = formattedLiveUrl;
+  } else {
+    alert("Recheck the Url");
+  }
+
   //   console.log(e.target.value);
 });
 
-// const downloadBtn = document.getElementById("btn");
-// downloadBtn.addEventListener("click", () => {
-//   var imageUrl = document.getElementById("image").src;
-//   document.getElementById("linkTag").href = imageUrl;
-//   document.getElementById("linkTag").click();
-// });
 //https://img.youtube.com/vi/lje9-Ft71IA/sddefault.jpg
 //https://youtu.be/OLZvAocak-g?si=LDkWNEV4QtvfDqqK
 //https://youtu.be/YAKMJpBWSqA?si=7dkYVaqDc_37FTf5
 //https://youtu.be/mMt0DY8szZk?si=tCyF7j1dfgrWUUp0
 // let a="YAKMJpBWSqA"
 // console.log(a.length)
+//https://www.youtube.com/watch?v=aGHVddspRq0&list=PL73Obo20O_7gcXt0cfQA14jey8zavtKAq&pp=iAQB
